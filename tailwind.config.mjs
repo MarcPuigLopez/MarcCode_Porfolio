2fb7e57fe78f159c1cd834607d9e4e80e7b3c7e9
Marc <marcpuig100@gmail.com>
2024-02-20 01:58:25 +0100

    Creación Pagina About Me

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 import { nextui } from "@nextui-org/react";
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
 	content: [
@@ -9,15 +10,18 @@ export default {
 	],
 	theme: {
 		extend: {
+			fontFamily: {
+				firaCode: ["Fira Code", ...defaultTheme.fontFamily.sans],
+			},
 			spacing: {
-				'100': '25rem',
-				'112': '28rem',
-				'120': '30rem',
-				'128': '32rem',
-				'144': '36rem',
-				'160': '40rem',
-				'176': '44rem',
-			}
+				100: "25rem",
+				112: "28rem",
+				120: "30rem",
+				128: "32rem",
+				144: "36rem",
+				160: "40rem",
+				176: "44rem",
+			},
 		},
 	},
 	darkMode: "class",
